e98b79b9dfe3ac42cef3bfcb06ac97645b974d45
RUBAN KARTHICK V <124701104+rubankarthick@users.noreply.github.com>
2024-04-11 11:17:24 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -13,7 +13,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                    InlineKeyboardButton("🎬 MAIN CHANNEL 🎬", url="https://t.me/+6pdw2jn048w1Zjg1"),
+                    InlineKeyboardButton("🎬 MAIN CHANNEL 🎬", url="https://t.me/+gqpU_4qsYAFmY2U1),
                         InlineKeyboardButton("🔒 CLOSE", callback_data = "close")
                     ]
                 ]
@@ -26,19 +26,6 @@ async def cb_handler(client: Bot, query: CallbackQuery):
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
-                        InlineKeyboardButton("📽️NEW TAMIL MOVIES🎬", url=f"https://t.me/+GiydT_oyMZE0MjZl")
-                    ],
-                    [
-                        InlineKeyboardButton("📽️HOLLYWOOD DUBBED MOVIES🎬", url=f"https://t.me/+r7uCG7sSC-VmMGE9")
-                    ],
-                    [
-                        InlineKeyboardButton("📽️MULTI LANGUAGE MOVIES🎬", url=f"https://t.me/+8pc1V0Mk8mM0Y2I1")
-                    ],
-                    [
-                        InlineKeyboardButton("📽️ALL NEW SERIES🎬", url=f"https://t.me/+Md4DpWxJ7NlmOTI9")
-                    ],
-                    [
-                        InlineKeyboardButton("🔉 DISCUSSION 🔉", url="https://t.me/+rucqp8Ao-soyMDU1"),
                         InlineKeyboardButton("🔒 CLOSE", callback_data = "close")
                     ]
                 ]
